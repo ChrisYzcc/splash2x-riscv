424db672f539c9fdbff303817f9f66c4af61ddae
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-11-30 13:18:37 +0800

    add water_spatial

---
+++ water_spatial/run.sh
@@ -0,0 +1,26 @@
+BENCH_DIR="$(pwd)"
+RUN_DIR=${BENCH_DIR}/build/${PLATFORM}/run
+
+if [ ! -d "${RUN_DIR}" ]; then
+    mkdir -p "${RUN_DIR}"
+else
+    rm -rf "${RUN_DIR}"/*
+fi
+
+# Get input files
+echo "Extracting input files ..."
+tar -xvf ${BENCH_DIR}/inputs/input_${INPUT_SIZE}.tar -C ${RUN_DIR}
+echo "Completed extracting input files."
+echo
+
+# Generate input file
+INPUT_FILE="${RUN_DIR}/input"
+INPUT_TEMPLATE="${RUN_DIR}/input.template"
+echo "Generating input file ${INPUT_FILE} ..."
+cat ${INPUT_TEMPLATE} | sed s/NUMPROCS/$NTHREADS/ > ${INPUT_FILE}
+echo
+
+echo "Running water_spatial..."
+cd ${RUN_DIR}
+${BENCH_DIR}/build/${PLATFORM}/bin/water_spatial ${NTHREADS} < ${INPUT_FILE}
+cd ${BENCH_DIR}
