8e02c124c602d0be47f736d21191b073c97341c8
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-12-01 21:17:41 +0800

    add script for getting sim size inputs

---
+++ gen_inputs.sh
@@ -0,0 +1,4 @@
+echo "Downloading splash2x sim size inputs..."
+wget https://github.com/ChrisYzcc/splash2x-riscv/releases/download/v0.0.1/splash2x-sim-inputs.tar.gz -O splash2x-sim-inputs.tar.gz
+tar -xzvf splash2x-sim-inputs.tar.gz --strip-components=1
+echo
