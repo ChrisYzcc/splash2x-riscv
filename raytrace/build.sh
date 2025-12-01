9764e1ec2b8b68aabd2d54e83ae9151cf97b573b
ChrisYzcc <yangzechen21@mails.ucas.ac.cn>
2025-12-01 20:18:51 +0800

    fix: raytrace multiple definition

--- raytrace/build.sh
+++ raytrace/build.sh
@@ -1,7 +1,5 @@
 BENCH_DIR="$(pwd)"
 
-export CFLAGS="${CFLAGS} -Wl,--allow-multiple-definition"
-
 mkdir -p ${BENCH_DIR}/build/${PLATFORM}/obj
 cp -r ${BENCH_DIR}/src/* ${BENCH_DIR}/build/${PLATFORM}/obj/
 
